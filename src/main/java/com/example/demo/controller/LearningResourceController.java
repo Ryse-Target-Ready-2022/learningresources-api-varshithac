5c1cb88e1ed0a636aaff095b61c50bcbb56cc5b5
unknown <varshithac17012000@gmail.com>
2022-07-18 16:09:00 +0530

    Controller

---
+++ src/main/java/com/example/demo/controller/LearningResourceController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.LearningResource;
+import com.example.demo.service.LearningResourceService;
+
+@RestController
+@RequestMapping("/learningresources")
+public class LearningResourceController {
+	
+	@Autowired
+	LearningResourceService service;
+	
+	@GetMapping("/")
+	public List<LearningResource> getLearningResources(){
+       return service.getLearningResources();
+	}
+
+	@PostMapping("/")
+	public void saveLearningResources(@RequestBody List<LearningResource> list){
+	    service.saveLearningResources(list);
+	}
+	
+    @DeleteMapping("/{id}")
+    public String removeLearningResource(@PathVariable int id){
+		 return service.removeLearningResource(id);
+	}
+
+}
