d97df9c3449544f0ce54dbc286cb70b67e2c45dd
unknown <varshithac17012000@gmail.com>
2022-07-18 16:06:06 +0530

    JPA

--- src/main/java/com/example/demo/entity/LearningResource.java
+++ src/main/java/com/example/demo/entity/LearningResource.java
@@ -2,20 +2,46 @@ package com.example.demo.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "learningresources")
 public class LearningResource {
 	
+	 @Id
+	 @Column(name = "learning_resource_id")
 	 private int id;
-	    private String name;
-	    private double costPrice;
-	    private double sellingPrice;
-	    private LearningResourceStatus productStatus;
-	    private LocalDate createdDate;
-	    private LocalDate publishedDate;
-	    private LocalDate retiredDate;
+	 
+	 @Column(name = "learning_resource_name")
+	 private String name;
+	 
+	 @Column(name = "cost_price")
+	 private double costPrice;
+	 
+	 @Column(name = "selling_price")
+	 private double sellingPrice;
 
-	    public LearningResource(){
+	 @Column(name = "learning_resource_status")
+	 @Enumerated(EnumType.STRING)
+	 private LearningResourceStatus productStatus;
+	 
+	 @Column(name = "created_date")
+	 private LocalDate createdDate;
+	 
+	 @Column(name = "published_date")
+	 private LocalDate publishedDate;
+	 
+	 @Column(name = "retired_date")
+	 private LocalDate retiredDate;
 
-	    }
+	 public LearningResource(){
+
+	 }
 
 	    public LearningResource(int id, String name, double costPrice, double sellingPrice, LearningResourceStatus productStatus, LocalDate createdDate, LocalDate publishedDate, LocalDate retiredDate) {
 	        this.id = id;
@@ -97,7 +123,8 @@ public class LearningResource {
 			return "LearningResource [id=" + id + ", name=" + name + ", costPrice=" + costPrice + ", sellingPrice="
 					+ sellingPrice + ", productStatus=" + productStatus + ", createdDate=" + createdDate
 					+ ", publishedDate=" + publishedDate + ", retiredDate=" + retiredDate + "]";
-		}    
+		}
+	    
+	    
 
 }
-
--- src/main/java/com/example/demo/service/LearningResourceService.java
+++ src/main/java/com/example/demo/service/LearningResourceService.java
@@ -1,107 +1,63 @@
 package com.example.demo.service;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.PrintWriter;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.LearningResource;
-import com.example.demo.entity.LearningResourceStatus;
+import com.example.demo.repository.LearningResourceRepository;
 
-
-
+@Service
 public class LearningResourceService {
+	
+	   @Autowired
+	   LearningResourceRepository repo;
 
-	 public List<LearningResource> getLearningResources(){
-
-	        List<LearningResource> list = new ArrayList<>();
-	        try {
-	            File f = new File("LearningResources.csv");
-
-	            if (f.exists()) {
-	                BufferedReader fr = new BufferedReader(new FileReader("LearningResources.csv"));
-	                String line = null;
-	                while ((line = fr.readLine()) != null) {
-	                    String[] fields = line.split(",");
-
-	                    int id = Integer.parseInt(fields[0]);
-	                    String name = fields[1];
-	                    double cp = Double.parseDouble(fields[2]);
-	                    double sp = Double.parseDouble(fields[3]);
-	                    LearningResourceStatus status = LearningResourceStatus.valueOf(fields[4]);
-	                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-	                    LocalDate cd = LocalDate.parse(fields[5],dtf);
-	                    LocalDate pd = LocalDate.parse(fields[6],dtf);
-	                    LocalDate rd = LocalDate.parse(fields[7],dtf);
-
-	                    LearningResource lr = new LearningResource(id, name, cp, sp, status, cd, pd, rd);
-
-	                    list.add(lr);
-	                }
-	                fr.close();
-	            }
-	        }
-	        catch(Exception e){
-	            System.out.println(e);
-	        }
-	        return list;
+	   public List<LearningResource> getLearningResources(){
+            return repo.findAll();
 	    }
 
+	   
 	    public void saveLearningResources(List<LearningResource> list){
-	        try{
-	            PrintWriter pw = new PrintWriter(new FileOutputStream("LearningResources.csv"),true);
+	    	for(LearningResource lr : list) {
+	    		if(!repo.existsById(lr.getId())) {
+	    		    repo.save(lr);
+	    		}
+	    	}		
+	      }
+	    
+	   
+		public String removeLearningResource(int id){
+			if(repo.existsById(id)) {
+			    repo.deleteById(id);
+			    return "Successfully Deleted";
+			}
+			return "Invalid Id";
+		}
 
-	            for(LearningResource lr : list){
-	                
-	                pw.print(lr.getId());
-	                pw.print(",");
-	                pw.print(lr.getName());
-	                pw.print(",");
-	                pw.print(lr.getCostPrice());
-	                pw.print(",");
-	                pw.print(lr.getSellingPrice());
-	                pw.print(",");
-	                pw.print(lr.getProductStatus());
-	                pw.print(",");
-	                pw.print(lr.getCreatedDate());
-	                pw.print(",");
-	                pw.print(lr.getPublishedDate());
-	                pw.print(",");
-	                pw.print(lr.getRetiredDate());
 
-	                pw.flush();
-	                pw.println();
-	               
-	            }
-	            pw.close();
-	        }
-	        catch(Exception e){
-	            System.out.println(e);
-	        }
-	       
-	    }
-
+	   
 	    public List<Double> profitMargin(){
 	        List<LearningResource> list = getLearningResources();
 	        List<Double> pmList = list.stream().map(el -> (el.getSellingPrice()-el.getCostPrice())/el.getSellingPrice()).collect(Collectors.toList());
 	        return pmList;
 	    }
-
+	    
+	    
 	    public List<LearningResource> sortLearingResourcesByProfitMargin(){
 	        List<LearningResource> list = getLearningResources();
 	        Collections.sort(list, (lr1, lr2) -> {
 	        	Double pm1 = (lr1.getSellingPrice() - lr1.getCostPrice())/lr1.getSellingPrice();
-               Double pm2 = (lr2.getSellingPrice() - lr2.getCostPrice())/lr2.getSellingPrice();
+                Double pm2 = (lr2.getSellingPrice() - lr2.getCostPrice())/lr2.getSellingPrice();
 	        	return pm1.compareTo(pm2);
 	        	
 	        });
 	        return list;
 	    }
 }
+
+
+
