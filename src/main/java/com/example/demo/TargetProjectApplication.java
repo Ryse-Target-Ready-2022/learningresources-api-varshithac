ebd407ae1e1e5cd2211b72814fc7a3cc0e61c9b8
unknown <varshithac17012000@gmail.com>
2022-07-18 15:40:35 +0530

    Hello World

---
+++ src/main/java/com/example/demo/TargetProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TargetProjectApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
